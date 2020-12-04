73644497f1ea939cd83ce063678861b92acb7638
Martin Bobčík <bobcik.martin@seznam.cz>
2020-12-04 15:47:14 +0100

    added mysql connection
    finally

--- HotelIS/QueryModule/Program.cs
+++ HotelIS/QueryModule/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RelationalDatabaseConnector;
 
 namespace QueryModule
 {
@@ -10,6 +11,7 @@ namespace QueryModule
     {
         static void Main(string[] args)
         {
+            var x = new SQLConnect();
         }
     }
 }
--- HotelIS/RelationalDatabaseConnector/SQLConnect.cs
+++ HotelIS/RelationalDatabaseConnector/SQLConnect.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace RelationalDatabaseConnector
 {
     public class SQLConnect
     {
+        //private const string connectionString = @"Server=127.0.0.1;Database=hotelis;Uid=demo;Pwd=demo";
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+        private string port;
+        private string connectionString;
+        private string sslM;
+
+        public SQLConnect()
+        {
+            server = "localhost";
+            database = "hotelis";
+            user = "root";
+            password = "";
+            port = "3306";
+            sslM = "none";
+
+            connectionString = String.Format("server={0};port={1};user id={2}; password={3}; database={4}; SslMode={5}", server, port, user, password, database, sslM);
+
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+
     }
 }
